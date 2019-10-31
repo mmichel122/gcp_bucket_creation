fa502a4d4ba624c1dbe1bcd9c42ea773fd5409ed
Mikael Michelier <mikael.michelier@googlemail.com>
2019-10-31 09:25:56 +0000

    first commit

---
+++ storage.tf
@@ -0,0 +1,36 @@
+terraform {
+  backend "remote" {
+    organization = "modus-demo"
+    workspaces {
+      name = "gcp-bucket-qa-eu"
+    }
+  }
+}
+
+provider "google" {
+  credentials = "${file("./creds/serviceaccount.json")}"
+  project     = var.project_id
+  region      = var.location
+}
+
+resource "google_storage_bucket" "web-store" {
+  name     = "modus-demo-lab"
+  location = "EU"
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
+
+resource "google_storage_bucket_object" "files" {
+  name   = "index.html"
+  source = "./website/index.html"
+  bucket = google_storage_bucket.web-store.name
+}
+
+resource "google_storage_bucket_access_control" "public_rule" {
+  bucket = google_storage_bucket.web-store.name
+  role   = "READER"
+  entity = "allUsers"
+}
---
+++ vars.tf
@@ -0,0 +1,7 @@
+variable "project_id" {
+  default = "data-249214"
+}
+
+variable "location" {
+  default = "europe-west2"
+}
